4d9101cbebf08cf08738ff5c07c9c61dfb355c4d
panhao <315358257@qq.com>
2018-08-16 09:41:33 +0800

    1

---
+++ drcb-cloud-task/src/main/java/com/deqingbank/cloud/task/DrcbCloudTaskApplication.java
@@ -0,0 +1,12 @@
+package com.deqingbank.cloud.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrcbCloudTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrcbCloudTaskApplication.class, args);
+	}
+}
