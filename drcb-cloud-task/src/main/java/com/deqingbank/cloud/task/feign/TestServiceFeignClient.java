400b96c55ba89adbaae84d36d640024c30b76bdf
panhao <315358257@qq.com>
2018-08-20 17:41:52 +0800

    feign with hystrix

--- drcb-cloud-task/src/main/java/com/deqingbank/cloud/task/feign/TestServiceFeignClient.java
+++ drcb-cloud-task/src/main/java/com/deqingbank/cloud/task/feign/TestServiceFeignClient.java
@@ -4,12 +4,22 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("cloud-service-test")
+@FeignClient(name="cloud-service-test")
 public interface TestServiceFeignClient {
 
 /*	@GetMapping("/zkt/test")
-	public void test();
+	public void test(); 
 */	
 	@GetMapping("/zkt/{ipaddr}")
 	public void test(@PathVariable("ipaddr") String ipaddr);
+}
+
+
+class TestServiceFeignClientFallBack implements TestServiceFeignClient{
+
+	@Override
+	public void test(String ipaddr) {
+		System.out.println("fallback run!");
+	}
+	
 }
