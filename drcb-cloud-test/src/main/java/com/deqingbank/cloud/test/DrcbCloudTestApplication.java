fb78cc0f464ce1db0e53a0294a80e7013a3f25aa
panhao <315358257@qq.com>
2018-08-20 11:08:09 +0800

    drcb-cloud-test first commit.

---
+++ drcb-cloud-test/src/main/java/com/deqingbank/cloud/test/DrcbCloudTestApplication.java
@@ -0,0 +1,31 @@
+package com.deqingbank.cloud.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableFeignClients
+@RestController
+@RequestMapping("/zkt")
+public class DrcbCloudTestApplication {
+
+	private Logger logger = LoggerFactory.getLogger(DrcbCloudTestApplication.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DrcbCloudTestApplication.class, args);
+	}
+	
+	@RequestMapping("/{ipAddr}")
+	public void download(@PathVariable String ipAddr) {
+		logger.debug("{} start to download!",ipAddr);
+	}
+	
+	
+}
