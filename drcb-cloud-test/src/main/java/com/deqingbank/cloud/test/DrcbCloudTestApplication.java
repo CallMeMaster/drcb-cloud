01eaea030283e8085fad40dbaee8dbc502ff84df
panhao <315358257@qq.com>
2018-08-22 16:38:53 +0800

    add swagger2

--- drcb-cloud-test/src/main/java/com/deqingbank/cloud/test/DrcbCloudTestApplication.java
+++ drcb-cloud-test/src/main/java/com/deqingbank/cloud/test/DrcbCloudTestApplication.java
@@ -5,18 +5,23 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @SpringBootApplication
 @EnableFeignClients
 @RestController
 @RequestMapping("/zkt")
+@Api
 public class DrcbCloudTestApplication {
 
 	private Logger logger = LoggerFactory.getLogger(DrcbCloudTestApplication.class);
-	
+	private static int sleepThread = 0;
 	public static void main(String[] args) {
 		SpringApplication.run(DrcbCloudTestApplication.class, args);
 	}
@@ -25,16 +30,17 @@ public class DrcbCloudTestApplication {
 	 * if ip addr is 00,then it goes to sleep.
 	 * @param ipAddr
 	 */
-	@RequestMapping("/{ipAddr}")
-	public void download(@PathVariable String ipAddr) {
+	@ApiOperation(value="download service",notes="download mock service!")
+	@GetMapping("/{ipAddr}")
+	public String download(@PathVariable Long ipAddr) {
 		logger.debug("{} start to download!",ipAddr);
-		if("00".equals(ipAddr)) {
-			try {
-				logger.debug("Failed service");
-				Thread.sleep(999999999);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+		sleepThread ++;
+		logger.debug("Failed service {}",sleepThread);
+		try {
+			Thread.sleep(ipAddr);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
+		return "success";
 	}
 }
