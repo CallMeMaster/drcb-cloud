525697303d492b119f3909fd18a4d29aa8eb01bc
Pan Hao <Pan Hao@DESKTOP-3ADOUVR>
2018-09-19 21:35:28 +0800

    fix bug of call the taskservice in the thread. get the taskservice form the springcontext by ServiceLocator.

--- drcb-cloud-task/src/main/java/com/deqingbank/cloud/task/core/RunnableTask.java
+++ drcb-cloud-task/src/main/java/com/deqingbank/cloud/task/core/RunnableTask.java
@@ -2,15 +2,19 @@ package com.deqingbank.cloud.task.core;
 
 import java.util.Date;
 
+import com.deqingbank.cloud.task.ServiceLocator;
 import com.deqingbank.cloud.task.entity.Task;
-import com.deqingbank.cloud.task.repository.TaskRepository;
+import com.deqingbank.cloud.task.entity.TaskState;
+import com.deqingbank.cloud.task.service.TaskService;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Data
-public class RunnableTask implements Runnable{
+public class RunnableTask implements Runnable {
+	
+	
 	
 	private Task task;
 	
@@ -24,6 +28,19 @@ public class RunnableTask implements Runnable{
 	
 	@Override
 	public void run() {
+		getTaskService().updateState(this.getId(), TaskState.PROCESSING);
 		log.debug("task {} run on {}!",this.getId(),new Date());
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			log.debug("sdfsdfadf");
+			getTaskService().updateState(this.getId(), TaskState.ERROR);
+			e.printStackTrace();
+		}
+		getTaskService().updateState(this.getId(), TaskState.PENDING);
+	}
+	
+	private TaskService getTaskService() {
+		return (TaskService)ServiceLocator.getService("taskService");
 	}
 }
