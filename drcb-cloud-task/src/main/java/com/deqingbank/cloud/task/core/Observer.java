4769de5bf9c2c6838a4c1657c61898e6bf2741d8
31535 <31535@DESKTOP-Q5PPJ0V>
2018-09-20 08:24:06 +0800

    remove unusable class

--- drcb-cloud-task/src/main/java/com/deqingbank/cloud/task/core/Observer.java
+++
@@ -1,5 +0,0 @@
-package com.deqingbank.cloud.task.core;
-
-public interface Observer {
-	public void update(String message);
-}
--- drcb-cloud-task/src/main/java/com/deqingbank/cloud/task/core/Observerable.java
+++
@@ -1,8 +0,0 @@
-package com.deqingbank.cloud.task.core;
-
-public interface Observerable {
-
-	public void registerObserver(Observer o);
-	public void removeObserver(Observer o);
-	public void notifyObserver();
-}
